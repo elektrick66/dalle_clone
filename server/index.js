dbc8ef2c08c1f546cd766b3b911e1328161a0735
elektrick66 <louaymoussa2001@gmail.com>
2023-06-05 02:48:16 +0100

    mongodb config

--- server/index.js
+++ server/index.js
@@ -2,16 +2,27 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './mongodb/connect.js';
+
 dotenv.config();
 
 const app= express();
 app.use(cors());
-app.use(express.json({ limit: '50mb '}));
+app.use(express.json({ limit: '50mb'}));
 
-app.get('/', async(req,res)=>{
+app.get('/', async(req, res)=>{
     res.send('Hello from DALL-E!');
 })
 
 const startServer= async ()=>{
-    app.listen(8080, ()=>console.log('Server has started running on port '))
+
+    try{
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, ()=> {console.log('Server has started running on port http://localhost:8080')})
+    } catch (error){
+        console.log(error);
+    }
+
 }
+
+startServer();
